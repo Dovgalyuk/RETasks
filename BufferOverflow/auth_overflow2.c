f09cac4c171c71e13745f28c4985001303bb3982
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2021-10-15 10:52:20 +0300

    Added dynamic analysis task

--- BufferOverflow/auth_overflow2.c
+++ BufferOverflow/auth_overflow2.c
@@ -9,21 +9,23 @@ void granted()
 	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 }
 
-int check_authentication() {
+int check_authentication()
+{
 	char password_buffer[16];
 
 	scanf("%s", password_buffer);
-	
-	if(strcmp(password_buffer, "brillig") == 0)
+
+	if (strcmp(password_buffer, "brillig") == 0)
 		return 1;
-	if(strcmp(password_buffer, "outgrabe") == 0)
+	if (strcmp(password_buffer, "outgrabe") == 0)
 		return 1;
 
 	return 0;
 }
 
-int main(int argc, char *argv[]) {
-	if(check_authentication()) {
+int main(int argc, char *argv[])
+{
+	if (check_authentication()) {
 		granted();
 	} else {
 		printf("\nAccess Denied.\n");
---
+++ Dynamic/build.sh
@@ -0,0 +1,1 @@
+afl-gcc ../BufferOverflow/auth_overflow2.c -o auth_overflow2_afl
